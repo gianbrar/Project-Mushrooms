f28f097d16310040492f757413bb90bf352107f4
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-04-13 11:18:45 -0400

    Update Bohnanza.js

--- Bohnanza.js
+++ Bohnanza.js
@@ -1,8 +1,15 @@
 function setup() {
     textAlign(CENTER, CENTER);
-    createCanvas(innerWidth, innerHeight, WEBGL);
-
+    createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
+let rotator = 0;
+
 function draw() {
+    background(255);
+    stroke(0, 0, 0);
+    rotateX(rotator * 0.1);
+    rotateY(rotator * 0.02);
+    box();
+    rotator++;
 }
